254e446bd1d50ee2e4899bf9da2c3d3d951459a1
XhAfAn <116730623+XhAfAn1@users.noreply.github.com>
2025-08-02 17:10:27 +0600

    alert model update

--- lib/Class Models/alert.dart
+++ lib/Class Models/alert.dart
@@ -6,9 +6,13 @@ class AlertModel {
   final String userName;
   final String userPhone;
   final String? address; // Optional: reverse geocoded location
+  final String? pstation;
+  final String? hpital;
   final String? message;
+  final String? etype;
   final String status; // 'active', 'resolved', 'ignored'
   final Timestamp timestamp;
+  final Timestamp? safeTime;
   final int severity; // 1 (low) to 5 (extreme)
   final List<String>? responders; // Nearby users or admins who responded
   final Map<String, dynamic>? location;
@@ -19,9 +23,13 @@ class AlertModel {
     required this.userName,
     required this.userPhone,
     this.address,
+    this.pstation,
+    this.hpital,
     this.message,
+    this.etype,
     required this.status,
     required this.timestamp,
+    this.safeTime,
     required this.severity,
      this.responders,
     this.location
@@ -35,9 +43,13 @@ class AlertModel {
       userName: json['userName'] ?? '',
       userPhone: json['userPhone'] ?? '',
       address: json['address'] ?? '',
+      pstation: json['pstation'] ?? '',
+      hpital: json['hpital'] ?? '',
       message: json['message'] ?? '',
+      etype: json['etype'] ?? 'unknown',
       status: json['status'] ?? 'active',
       timestamp: json['timestamp'] ?? Timestamp.now(),
+      safeTime: json['safeTime'] ?? null,
       severity: json['severity'] ?? 1,
       responders: List<String>.from(json['responders'] ?? []),
       location: {
@@ -55,9 +67,13 @@ class AlertModel {
       'userName': userName,
       'userPhone': userPhone,
       'address': address,
+      'pstation': pstation,
+      'hpital': hpital,
       'message': message,
+      'etype': etype,
       'status': status,
       'timestamp': timestamp,
+      'safeTime': safeTime,
       'severity': severity,
       'responders': responders,
       'location': location,
